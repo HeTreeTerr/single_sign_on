a6ebba6c7175eec0d08268bdba640751e88ab070
hesensen <hesensen@hztianque.com>
2020-02-24 23:52:59 +0800

    sso-jwt开场

---
+++ hss-sso-jwt/make-jwt-token/src/main/java/hss/sso/jwt/MakeJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package hss.sso.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeJwtTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeJwtTokenApplication.class, args);
+    }
+
+}
