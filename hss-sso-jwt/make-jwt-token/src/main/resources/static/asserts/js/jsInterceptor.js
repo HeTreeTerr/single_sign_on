0138c2fb68f47dcd60442300f2e794ba947e0cde
hesensen <hesensen@hztianque.com>
2020-02-27 21:51:11 +0800

    js拦截ajax请求，每半小时刷新token

--- hss-sso-jwt/make-jwt-token/src/main/resources/static/asserts/js/jsInterceptor.js
+++ hss-sso-jwt/make-jwt-token/src/main/resources/static/asserts/js/jsInterceptor.js
@@ -14,7 +14,7 @@ hookAjax({
         return false;
         拦截
         return true;*/
-        debugger;
+
         //请求方式
         var method = arg[0];
         //请求路径
@@ -24,11 +24,58 @@ hookAjax({
         //将登录和退出放入白名单中,拦截其他方法
         var loginPath = new RegExp(projectName+"/login");
         var inValidPath = new RegExp(projectName+"/inValid");
-        if(!(loginPath.test(url)) && !(inValidPath.test(url))) {
+        var refreshJwtPath = new RegExp(projectName+"/refreshJwt");
+
+        if(!(loginPath.test(url)) && !(inValidPath.test(url)) && !(refreshJwtPath.test(url))) {
             console.log("js拦截器最终效果----》刷新token");
+            //1.获取token
+            var token = localStorage.getItem("token");
+            //2.判断本地浏览器有没有token,有则进行token刷新
+            if(token != null && token != "" && token != undefined){
+                console.log("当前浏览器存在token");
+                $.ajax({
+                    url: projectName+"/refreshJwt",
+                    data:"token="+token,
+                    type: "GET",
+                    dataType: "json",
+                    success: function(result) {
+                        console.log("返回结果：code="+result.code+"---data="+result.data);
+                        debugger;
+                        if(result.code == 1){
+                            //效验并刷新token成功
+                            if(token != result.data){
+                                //替换本地token
+                                localStorage.setItem("token",result.data);
+                                //删除远程redis-token
+                                clearRedisToken(token);
+                            }
+                        }else{
+                            alert("刷新token失败");
+                        }
+                    }
+                });
+            }
         }
     },
     send:function(arg){
         console.log("send called: %O",arg[0]);
     }
 });
+
+//清除redis中的密匙和token
+function clearRedisToken(token) {
+    $.ajax({
+        url: projectName+"/inValid",
+        data:"token="+token,
+        type: "GET",
+        dataType: "json",
+        success: function(result) {
+            console.log("返回结果：code="+result.code+"---data="+result.data);
+            if(result.code == 1){
+                console.log("清除redis-token成功");
+            }else{
+                alert("清除redis-token失败");
+            }
+        }
+    });
+}
